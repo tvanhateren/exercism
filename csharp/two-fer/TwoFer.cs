605d313bbd1e883dcc9bb706011232c5068b1cf6
Tom <exercism-test@mailforspam.com>
2020-10-12 17:00:23 +0200

    [TwoFer] use $ formatting + lamda style function

--- csharp/two-fer/TwoFer.cs
+++ csharp/two-fer/TwoFer.cs
@@ -2,10 +2,5 @@
 
 public static class TwoFer
 {
-    // In order to get the tests running, first you need to make sure the Speak method 
-    // can be called both without any arguments and also by passing one string argument.
-    public static string Speak(string name = "you")
-    {
-        return String.Format("One for {0}, one for me.", name);
-    }
+    public static string Speak(string name = "you") => $"One for {name}, one for me.";
 }
