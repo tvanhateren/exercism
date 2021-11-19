b05a420fd7dcb56e646ec59d5c11caade24920f4
Tom <exercism-test@mailforspam.com>
2021-11-19 14:17:29 +0000

    [PHP] Solve robot-simulator using strings
    The signature of the constructor has the direction as a string.
    Because numbers are more convenient I use the strings as raw bytes.

--- php/robot-simulator/RobotSimulator.php
+++ php/robot-simulator/RobotSimulator.php
@@ -1,31 +1,25 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
+
+function mod(int $num, int $mod): int
+{
+    $result = $num % $mod;
+
+    if ($result < 0) {
+        $result += $mod;
+    }
+
+    return $result;
+}
 
 class Robot
 {
+    const DIRECTION_NORTH = "\0";
+    const DIRECTION_EAST = "\1";
+    const DIRECTION_SOUTH = "\2";
+    const DIRECTION_WEST = "\3";
+
     /**
      *
      * @var int[]
@@ -40,21 +34,69 @@ class Robot
 
     public function __construct(array $position, string $direction)
     {
-        throw new \BadMethodCallException("Implement the __construct method");
+        $this->position = $position;
+        $this->direction = $direction;
+    }
+
+    public function __get($name)
+    {
+        switch ($name) {
+            case "position":
+                return $this->position;
+            case "direction":
+                return $this->direction;
+        }
     }
 
     public function turnRight(): self
     {
-        throw new \BadMethodCallException("Implement the turnRight method");
+        $this->direction = chr(mod(ord($this->direction) + 1, 4));
+        return $this;
     }
 
     public function turnLeft(): self
     {
-        throw new \BadMethodCallException("Implement the turnLeft method");
+        $this->direction = chr(mod(ord($this->direction) - 1, 4));
+        return $this;
     }
 
     public function advance(): self
     {
-        throw new \BadMethodCallException("Implement the advance method");
+        switch ($this->direction) {
+            case self::DIRECTION_NORTH:
+                $this->position[1] += 1;
+                break;
+            case self::DIRECTION_EAST:
+                $this->position[0] += 1;
+                break;
+            case self::DIRECTION_SOUTH:
+                $this->position[1] -= 1;
+                break;
+            case self::DIRECTION_WEST:
+                $this->position[0] -= 1;
+                break;
+        }
+        return $this;
+    }
+
+    public function instructions(string $commands): self
+    {
+        foreach (mb_str_split($commands) as $command) {
+            switch ($command) {
+                case "L":
+                    $this->turnLeft();
+                    break;
+                case "R":
+                    $this->turnRight();
+                    break;
+                case "A":
+                    $this->advance();
+                    break;
+                default:
+                    throw new InvalidArgumentException("Invalid instruction: " . $command);
+            }
+        }
+
+        return $this;
     }
 }
