88ca9d13c29d682653016b8e8340dabc035cdf5c
Tom <exercism-test@mailforspam.com>
2021-11-20 18:39:59 +0000

    [PHP] Solve atbash-cipher using strtr
    Replace preg_replace with strtr.
    Replace chunk_split with wordwrap.

--- php/atbash-cipher/AtbashCipher.php
+++ php/atbash-cipher/AtbashCipher.php
@@ -5,10 +5,7 @@ declare(strict_types=1);
 function encode(string $text): string
 {
     $text = strtolower(preg_replace("/[^a-zA-Z\d]+/", "", $text));
-    $text = preg_replace_callback(
-        "/[a-z]/",
-        fn ($c) => chr((25 - (ord($c[0]) - ord('a')) % 26) + ord('a')),
-        $text
-    );
-    return substr(chunk_split($text, 5, " "), 0, -1);
+    $chars = implode('', range('a', 'z'));
+    $text = strtr($text, $chars, strrev($chars));
+    return wordwrap($text, 5, " ", true);
 }
