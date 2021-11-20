04be0b36b9a57e54049f666db0a3de857884d2b0
Tom <exercism-test@mailforspam.com>
2021-11-20 16:44:36 +0000

    [PHP] Solve anagram using count_chars
    Initial attempt used own implementation until I learned of this function

--- php/anagram/Anagram.php
+++ php/anagram/Anagram.php
@@ -1,30 +1,25 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
+
+function word_info($word)
+{
+    $word = mb_convert_case($word, MB_CASE_LOWER);
+    $count = count_chars($word, 1);
+    return [$word, $count];
+}
 
 function detectAnagrams(string $word, array $anagrams): array
 {
-    throw new \BadFunctionCallException("Implement the detectAnagrams function");
+    $result = [];
+
+    $a = word_info($word);
+    foreach ($anagrams as $other_word) {
+        $b = word_info($other_word);
+        if ($a[0] != $b[0] && $a[1] == $b[1]) {
+            $result[] = $other_word;
+        }
+    }
+
+    return $result;
 }
